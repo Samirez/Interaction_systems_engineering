32c3b603b00fda39fb3f545ad72738e62aec45e6
Ahmadullah Naibi <ahnai17@student.sdu.dk>
2022-10-26 10:30:31 +0200

    Update urls.py

--- real_estate_site/real_estate_site/urls.py
+++ real_estate_site/real_estate_site/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from .views import (home_view)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', home_view, name='home')
 ]
