64086271a0cfd26489bc286fc6538b493cd1c39a
Ahmadullah Naibi <ahnai17@student.sdu.dk>
2022-11-16 19:41:19 +0100

    added static directory for css and javascript

--- real_estate_site/real_estate_site/settings.py
+++ real_estate_site/real_estate_site/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static_proj",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
