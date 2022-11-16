76854ba5902e02b1ed2982990c27f8954fbc512d
Ahmadullah Naibi <ahnai17@student.sdu.dk>
2022-11-16 15:05:53 +0100

    server is running
    views py needs changes applied to it

--- real_estate_site/real_estate_site/urls.py
+++ real_estate_site/real_estate_site/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url
-from .views import (home_view)
+from django.urls import include, re_path
+#from .views import (home_view)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^$', home_view, name='home')
+    re_path('admin/', admin.site.urls),
+    #re_path(r'^$', home_view, name='home')
 ]
