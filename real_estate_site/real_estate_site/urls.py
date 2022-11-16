a61ef2477790ff01abc81c6f2023f1c356e2e727
Ahmadullah Naibi <ahnai17@student.sdu.dk>
2022-11-16 19:17:36 +0100

    fixed url

--- real_estate_site/real_estate_site/urls.py
+++ real_estate_site/real_estate_site/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, re_path
-#from .views import (home_view)
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    re_path('admin/', admin.site.urls),
-    #re_path(r'^$', home_view, name='home')
+    path('admin/', admin.site.urls),
+    path(r'^/real_estate/home$', views.home_view, name='home')
 ]
--- real_estate_site/real_estate_site/views.py
+++ real_estate_site/real_estate_site/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
+def home_view(request):
         return render(request, "Real Estate/Home.html")
